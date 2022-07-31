617404cfc72b6e0f111a1010f7739e72dfd2e7d0
NoahDake <109688893+NoahDake@users.noreply.github.com>
2022-07-31 13:09:25 -0700

    Add files via upload

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,36 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+
+let greetings= "Hello! Fellow Humans";
+let name= 'hon';
+let alsoKnownAs= " The goddess of ice";
+let palnetName= 'Mars';
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+
+let year= 3075;
+let age= 2089;
+let noOfMemebers= 7;
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+
+let supplies= ['shelter','water','food']
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+
+let statement= true;
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+
+console.log(greetings + "." + " My name is " + name + ".")
+console.log("Also known as," + alsoKnownAs + ".")
+console.log("The year right now is " + year + ",")
+console.log("making me " + age + " years old" + ".")
+console.log("Currently me and my team of " + noOfMemebers + ", are on a planet known as " + palnetName + ".")
+console.log("We are short on " + supplies[2] + ", that my human friends said is very much needed for their survival")
+console.log("Which is " + statement + " as they need for energy and to stay alive")
+console.log("I'll be looking forward to seeing you, and hopefully you bring us the supplies soon")
