882fa0151f8086d65350bdf812917c4dea008f8d
NoahDake <109688893+NoahDake@users.noreply.github.com>
2022-07-31 13:10:27 -0700

    Add files via upload

---
+++ Creating Sentences Activity/creating_sentences.js
@@ -0,0 +1,26 @@
+// TODO: Create an array variable called "words" with the following elements: 'This', 'is', 'JavaScript', 'Coding!'
+// YOUR CODE GOES HERE
+
+
+// TODO: Create an empty string variable called "sentence".
+// YOUR CODE GOES HERE
+
+
+// TODO: 
+// 1. Create a function called "createSentence" that takes an array as an argument.
+// 2. In the function use a for loop to iterate through each word element of the array.
+// 3. Add each word to the "sentence" variable.
+// 4. Return the "sentence".
+// YOUR CODE GOES HERE
+let words = ['This', 'is', 'Javascript', 'Coding!'];
+console.log(words)
+var sentance = "";
+
+for (let i = 0; i < words.leangh; i++){
+    sentance += words[i];
+    
+}
+console.log(sentance)
+
+// TODO: Call the function "createSentence" using the console.log method.
+// YOUR CODE GOES HERE
